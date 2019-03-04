df5e0e976ff94f2e768d70da76da517668712187
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-03-04 14:49:58 +0000

    integrated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :boxes, only: [:index, :show] do
+    resources :subscriptions, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
+  end
+  resources :challenges, only: [:index, :show, :edit, :update, :destroy]
+  resources :subscriptions, only: [:index, :show, :edit, :update, :destroy]
 end
