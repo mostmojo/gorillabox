70488e7ef12aa2f008259e50181e268e20491417
Tasneem Karwa <tasneemkarwa@gmail.com>
2019-03-05 14:22:05 +0000

    product card added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'boxes#index'
   resources :boxes, only: [:index, :show] do
     resources :subscriptions, only: [:new, :create]
     resources :reviews, only: [:new, :create]
