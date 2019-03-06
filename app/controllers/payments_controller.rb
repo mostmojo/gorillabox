9d62112c1129605c263ae3135a0da87ebc94f87b
Omar Berrada <berradaomar@yahoo.com>
2019-03-06 16:39:04 +0000

    small indentations

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,30 +6,28 @@ class PaymentsController < ApplicationController
 
   def create
     customer = Stripe::Customer.create(
-    source: params[:stripeToken],
-    email:  params[:stripeEmail]
+      source: params[:stripeToken],
+      email: params[:stripeEmail]
     )
 
     charge = Stripe::Charge.create(
-    customer:     customer.id,   # You should store this customer id and re-use it.
-    amount:       @subscription.amount_cents,
-    description:  "Payment for Box #{@subscription.box_sku} for subscription #{@subscription.id}",
-    currency:     @subscription.amount.currency
+      customer: customer.id, # You should store this customer id and re-use it.
+      amount: @subscription.amount_cents,
+      description: "Payment for Box #{@subscription.box_sku} for subscription #{@subscription.id}",
+      currency: @subscription.amount.currency
     )
 
     @subscription.update(payment: charge.to_json, state: 'paid')
     current_user.has_subscription = true
-    raise
     redirect_to subscription_path(@subscription)
-
-    rescue Stripe::CardError => e
+  rescue Stripe::CardError => e
     flash[:alert] = e.message
     redirect_to new_subscription_payment_path(@subscription)
   end
 
-    private
+  private
 
-    def set_subscription
-      @subscription = current_user.subscriptions.where(state: 'pending').find(params[:subscription_id])
-    end
+  def set_subscription
+    @subscription = current_user.subscriptions.where(state: 'pending').find(params[:subscription_id])
+  end
 end
