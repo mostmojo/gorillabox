773c5d226202efa6603f378083ee1781308ee469
Justas Jaskunas <jaskunas.justas@gmail.com>
2019-03-07 14:16:52 +0000

    Created form for users update their profile information

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import { initAutocomplete } from "../plugins/init_autocomplete.js"
 import { update_total_amount } from "../plugins/update_total_amount.js"
+import "../plugins/flatpickr";
+
 
 initAutocomplete();
 update_total_amount();
