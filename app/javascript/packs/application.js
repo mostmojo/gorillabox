9b22d9d2a8d191a6ad3fc1ea45331de759a98822
Omar Berrada <berradaomar@yahoo.com>
2019-03-07 19:36:52 +0000

    added map to the invoice page with a pin on the delivery adress

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,13 +5,13 @@ import "../plugins/flatpickr";
 import { smooth_scroll_application } from "../plugins/smooth_scroll_application.js"
 import { review_stars } from "../plugins/review_stars.js"
 import { reviews_sliding } from "../plugins/reviews_sliding.js"
-import 'mapbox-gl/dist/mapbox-gl.css';
-import { initMapbox } from '../plugins/init_mapbox.js';
+import 'mapbox-gl/dist/mapbox-gl.css'
+import { init_mapbox } from '../plugins/init_mapbox.js'
 
+init_mapbox();
 initAutocomplete();
 update_total_amount();
 smooth_scroll_application();
 review_stars();
 reviews_sliding();
-initMapbox();
 
