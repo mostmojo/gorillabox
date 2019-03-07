35f7dddb8a9ad46b34d2eea4b900a2e078801fbd
Omar Berrada <berradaomar@yahoo.com>
2019-03-07 11:20:12 +0000

    reviews star system on going

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import { initAutocomplete } from "../plugins/init_autocomplete.js"
 import { update_total_amount } from "../plugins/update_total_amount.js"
+import { review_stars } from "../plugins/reviews.js"
 
 initAutocomplete();
 update_total_amount();
+review_stars();
---
+++ app/javascript/plugins/reviews.js
@@ -0,0 +1,13 @@
+
+const review_stars = () => {
+  const reviewInput = document.querySelector("#review_stars")
+
+  const stars = document.querySelectorAll('.rating > input')
+  stars.forEach(star => {
+    star.addEventListener('click', event => {
+      reviewInput.value = event.currentTarget.value
+    })
+  })
+}
+
+export { review_stars };
