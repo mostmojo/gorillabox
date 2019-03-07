b6e05a6a1afa1d5b23a32638087647cb9123f357
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-03-07 11:19:05 +0000

    JS refactor

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import { initAutocomplete } from "../plugins/init_autocomplete.js"
 import { update_total_amount } from "../plugins/update_total_amount.js"
+import { smooth_scroll_application } from "../plugins/smooth_scroll_application.js"
 
 initAutocomplete();
 update_total_amount();
+smooth_scroll_application();
--- app/javascript/plugins/update_total_amount.js
+++ app/javascript/plugins/update_total_amount.js
@@ -1,18 +1,24 @@
 const update_total_amount = () => {
 
-  const boxPrice = document.getElementById('hidden_price').innerHTML
+  const hiddenPrice = document.getElementById('hidden_price')
+  if (hiddenPrice) {
+    let boxPrice = hiddenPrice.innerHTML
+  }
   const quantityBar = document.getElementById("subscription_quantity")
 
-  quantityBar.addEventListener('blur', (event) => {
-    const quantity = event.srcElement.valueAsNumber;
-    const total = (boxPrice * quantity)
-    if (isNaN(total)) {
-      document.getElementById('total').innerHTML = `£0`;
-    } else {
-      document.getElementById('total').innerHTML = `£${total}`;
-    }
+  if (quantityBar) {
+    quantityBar.addEventListener('blur', (event) => {
+      const quantity = event.srcElement.valueAsNumber;
+      const total = (boxPrice * quantity)
+      if (isNaN(total)) {
+        document.getElementById('total').innerHTML = `£0`;
+      } else {
+        document.getElementById('total').innerHTML = `£${total}`;
+      }
 
-  })
+    })
+    
+  }
 };
 
 export { update_total_amount };
