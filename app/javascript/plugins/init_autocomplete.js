8b9845a346a4c8f9beee78d28f9a15364625edad
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-03-13 17:01:34 +0000

    fixed places

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,5 +1,5 @@
 // app/javascript/plugins/init_autocomplete.js
-// import places from 'places.js';
+import places from 'places.js';
 
 const initAutocomplete = () => {
   const addressInput = document.getElementById('subscription_delivery_address');
