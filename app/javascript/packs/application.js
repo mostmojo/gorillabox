512f1666acfbb68159b661ad48b5b6615ebbf824
Omar Berrada <berradaomar@yahoo.com>
2019-03-13 16:43:52 +0000

    fixed map bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,11 @@
-import "bootstrap";
+// import "bootstrap";
 
-import { initAutocomplete } from "../plugins/init_autocomplete.js"
-import { update_total_amount } from "../plugins/update_total_amount.js"
-import { smooth_scroll_application } from "../plugins/smooth_scroll_application.js"
-import { review_stars } from "../plugins/review_stars.js"
-import { reviews_sliding } from "../plugins/reviews_sliding.js"
-import 'mapbox-gl/dist/mapbox-gl.css'
-import { init_mapbox } from '../plugins/init_mapbox.js'
+import { initAutocomplete } from "../plugins/init_autocomplete.js";
+import { update_total_amount } from "../plugins/update_total_amount.js";
+import { smooth_scroll_application } from "../plugins/smooth_scroll_application.js";
+import { review_stars } from "../plugins/review_stars.js";
+import { reviews_sliding } from "../plugins/reviews_sliding.js";
+import { init_mapbox } from '../plugins/init_mapbox.js';
 // Profile tabs
 import { openCity } from '../components/tabs';
 
@@ -20,4 +19,4 @@ initAutocomplete();
 update_total_amount();
 smooth_scroll_application();
 review_stars();
-reviews_sliding();
+// reviews_sliding();
