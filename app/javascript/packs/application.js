d71ca821f3248ff2c32738e5e68d5d856cea4ecc
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-03-14 11:41:24 +0000

    webpack version

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,34 +1,24 @@
-import "bootstrap";
+// import "bootstrap";
 
-import { smooth_scroll_application } from "../plugins/smooth_scroll_application.js";
-import { review_stars } from "../plugins/review_stars.js";
+// import { smooth_scroll_application } from "../plugins/smooth_scroll_application.js";
+// import { review_stars } from "../plugins/review_stars.js";
 
-smooth_scroll_application();
-review_stars();
-
-
+// smooth_scroll_application();
+// review_stars();
 
 
-
-
-
-
-
-
-
-
-import { initAutocomplete } from "../plugins/init_autocomplete.js";
-import { update_total_amount } from "../plugins/update_total_amount.js";
-// import { reviews_sliding } from "../plugins/reviews_sliding.js";
-import { init_mapbox } from '../plugins/init_mapbox.js';
-// Profile tabs
-import { openCity } from '../components/tabs';
-// import {images_expanding } from '../plugins/images_expanding';
+// import { initAutocomplete } from "../plugins/init_autocomplete.js";
+// import { update_total_amount } from "../plugins/update_total_amount.js";
+// // import { reviews_sliding } from "../plugins/reviews_sliding.js";
+// import { init_mapbox } from '../plugins/init_mapbox.js';
+// // Profile tabs
+// import { openCity } from '../components/tabs';
+// // import {images_expanding } from '../plugins/images_expanding';
 
 
 
-init_mapbox();
-initAutocomplete();
-update_total_amount();
-// reviews_sliding();
-// images_expanding();
+// init_mapbox();
+// initAutocomplete();
+// update_total_amount();
+// // reviews_sliding();
+// // images_expanding();
